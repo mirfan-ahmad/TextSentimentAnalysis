3ca55038731c0bb1e30c2810cbb311b9b5e161bb
Irfan Ahmad <irfanahmad2959@gmail.com>
2025-01-04 15:36:27 +0500

    gradio app

---
+++ app.py
@@ -0,0 +1,17 @@
+from transformers import pipeline
+import gradio as gr
+
+
+model = pipeline("text-classification")
+def predict(text):
+    try:
+        result = model(text)
+        return result[0]['label']
+    except Exception as e:
+        return str(e)
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter your text", lines=10)
+    demo = gr.Interface(fn=predict, inputs=textbox, outputs="text")
+    
+demo.launch()
